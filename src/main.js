233e5a95412841f31dce87d2f2698a24a8ed7823
peterturn <peterturnmay@gmail.com>
2024-03-09 10:51:21 +0100

    prettier

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import router from './router';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import store from './store';
-import {globalMixin} from '@/mixins/globalMix'
+import { globalMixin } from '@/mixins/globalMix'
 import permissions from './directives/permissions';
 import { changeStringFilter } from './filters/changeStringFilter';
 Vue.config.productionTip = false
 Vue.use(Buefy)
 
 Vue.mixin(globalMixin)
-Vue.directive('permissions', permissions )
+Vue.directive('permissions', permissions)
 Vue.filter('changeStringFilter', changeStringFilter)
 
 new Vue({
